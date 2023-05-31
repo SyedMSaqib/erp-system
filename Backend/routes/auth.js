ec5cf9395575ef78c8c2d9a6d9355a554c361314
Saqib <splasher733@gmail.com>
2023-05-31 22:10:55 +0500

    doing work on signup authentication

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const router = express.Router()
-
+const user=require("../models/users")
+const { query, validationResult } = require('express-validator');
 
 
 //Signup authentication
-router.get('/auth/signup', (req, res) => {
-  res.send('')
+router.get('/auth/signup',query("name").notEmpty,query("email").isEmail,query("password").isLength({min:6})
+ ,async(req, res) => {
+    const result= validationResult(req)
+    if(result)
+        return res.status(401).send("Provide correct data for all fields")
+    
+    const checkUser=await user.findOne(req.body.email)
+    if(checkUser)
+    res.send.status(403).send.json({"error":"User already exists"})
+    const {name,email,password}=req.body
+    
+
+    
+  
 })
 
 
