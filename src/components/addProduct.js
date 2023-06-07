6e56cbee1c3dbc86b9681bda6800c9e6f5bc0f01
Saqib <splasher733@gmail.com>
2023-06-08 02:08:59 +0500

    some bugs fixed of add product

--- src/components/addProduct.js
+++ src/components/addProduct.js
@@ -4,7 +4,7 @@ const AddProduct = () => {
   return (
     
 
-    <div className="w-full max-w-lg lg:pl-40 pt-20 md:pl-0 sm:pl-0">
+    <div className="w-full ml-52 max-w-lg lg:ml-96   pt-20 md:pl-0 sm:pl-0">
     <h1 className='font-semibold'>Add Product</h1>
 <form className='pt-10'>
     
@@ -35,7 +35,7 @@ const AddProduct = () => {
   </div>
 
   
-  <div className='lg:pl-32 sm:pl-0'>
+  <div className='lg:ml-48 sm:ml-96 md:ml-36'>
   <button
   type="button"
   class="  inline-block rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-slate-400 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]">
