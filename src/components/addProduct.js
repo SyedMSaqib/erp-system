96719be5fc634589099d2030cf07c6a1e312330c
Saqib <splasher733@gmail.com>
2023-06-08 00:34:40 +0500

    button add to add product

--- src/components/addProduct.js
+++ src/components/addProduct.js
@@ -4,9 +4,10 @@ const AddProduct = () => {
   return (
     
 
-
-
-<form className="w-full max-w-lg lg:pl-40 pt-20 md:pl-0 sm:pl-0">
+    <div className="w-full max-w-lg lg:pl-40 pt-20 md:pl-0 sm:pl-0">
+    <h1 className='font-semibold'>Add Product</h1>
+<form className='pt-10'>
+    
   
   <div className="flex flex-wrap -mx-3 mb-6">
     <div className="w-full px-3">
@@ -28,15 +29,24 @@ const AddProduct = () => {
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
         Category
       </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="description" placeholder=""/>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="description" placeholder="" minLength={5}/>
     
     </div>
   </div>
-    
-    </div>
-  </div>
+
   
+  <div className='lg:pl-32 sm:pl-0'>
+  <button
+  type="button"
+  class="  inline-block rounded bg-neutral-50 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-800 shadow-[0_4px_9px_-4px_#cbcbcb] transition duration-150 ease-in-out hover:bg-slate-400 hover:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:bg-neutral-100 focus:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-200 active:shadow-[0_8px_9px_-4px_rgba(203,203,203,0.3),0_4px_18px_0_rgba(203,203,203,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(251,251,251,0.3)] dark:hover:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:focus:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)] dark:active:shadow-[0_8px_9px_-4px_rgba(251,251,251,0.1),0_4px_18px_0_rgba(251,251,251,0.05)]">
+  Submit
+</button>
+      </div>
+    
+    </div>
+  </div>
 </form>
+  </div>
 
 
   )
