2672cfde965e70d1a43288e3c1c5da927432b8b9
Saqib <splasher733@gmail.com>
2023-06-08 02:08:23 +0500

    product card added

--- src/components/viewProduct.js
+++ src/components/viewProduct.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Card from './viewProductCard';
 
 const ViewProduct = () => {
   return (
-    <div className='pl-10'>ViewProduct</div>
-  )
-}
+    <div className='flex flex-wrap ml-52 '>
+      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
+        <Card />
+      </div>
+      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
+        <Card />
+      </div>
+      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
+        <Card />
+      </div>
+      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
+        <Card />
+      </div>
+      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
+        <Card />
+      </div>
+      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
+        <Card />
+      </div>
+    </div>
+  );
+};
 
-export default ViewProduct
+export default ViewProduct;
