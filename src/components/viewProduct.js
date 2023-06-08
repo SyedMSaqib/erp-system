ce0a817fec85f39dbda6cd1ebc52eee98ea3147d
Saqib <splasher733@gmail.com>
2023-06-08 19:49:01 +0500

    kuch khas ni

--- src/components/viewProduct.js
+++ src/components/viewProduct.js
@@ -11,23 +11,6 @@ const ViewProduct = () => {
         {product.map((product)=>{
          return <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1 '> <Card key={product._id} data={product}/></div>
         })}
-        
-      
-      {/* <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
-        <Card />
-      </div>
-      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
-        <Card />
-      </div>
-      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
-        <Card />
-      </div>
-      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
-        <Card />
-      </div>
-      <div className='lg:w-1/3 p-4 md:w-1/2 sm:w-1/1'>
-        <Card />
-      </div> */}
     </div>
   );
 };
