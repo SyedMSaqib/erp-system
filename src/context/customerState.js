c2599a27608b2195623b18202affa0908da8f93a
Saqib <splasher733@gmail.com>
2023-09-23 23:19:25 +0500

    customerContext created and customerState

---
+++ src/context/customerState.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import customerContext from './productContext'
+
+
+const customerState = (props) => {
+    const host="http://localhost:5000"
+    const customerDb=
+        []
+
+      const [customer, setCustomer] = useState(customerDb)
+      const [customerId, setCustomerId] = useState(null)
+      const [updateFormValues, setupdateFormValues] = useState(null)
+      
+      
+      const getAllcustomers=async()=>{
+        //get notes Api call
+        
+        const response = await fetch(`${host}/customer/fetchAllCustomer`, {
+            method: "GET", 
+             
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ3ZTRlNTYyZThjNWNhZmRmZWY2MzdmIn0sImlhdCI6MTY4NTk5OTE5MH0.tANfZWBhWrVuSXEhNvIAutanlz2LpwO0ZaJrDfN3cj0"
+              
+            },
+            
+          });
+          const data=await response.json(); 
+          setCustomer(data)
+        
+
+      }
+    
+    
+      const addCustomer=async(name,phone,email)=>{
+        
+        const newCustomer={
+            name: name,
+            phone: phone,
+            email: email,
+          }
+          setCustomer(customer.concat(newCustomer))
+        
+        //Add customer Api call
+        
+            const response = await fetch(`${host}/customer/addCustomer`, {
+              method: "POST", 
+               
+              headers: {
+                "Content-Type":"application/json",
+                "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ3ZTRlNTYyZThjNWNhZmRmZWY2MzdmIn0sImlhdCI6MTY4NTk5OTE5MH0.tANfZWBhWrVuSXEhNvIAutanlz2LpwO0ZaJrDfN3cj0"
+                
+              },
+              body: JSON.stringify({name,email,phone}),
+            });
+        
+    }
+
+    const updateCustomer=async(name,email,phone)=>{
+          
+      //Update customer Api call
+      
+        
+          const response = await fetch(`${host}/customer/updateCustomer/${id}`, {
+            method: "PUT", 
+             
+            headers: {
+              "Content-Type":"application/json",
+              "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ3ZTRlNTYyZThjNWNhZmRmZWY2MzdmIn0sImlhdCI6MTY4NTk5OTE5MH0.tANfZWBhWrVuSXEhNvIAutanlz2LpwO0ZaJrDfN3cj0"
+              
+            },
+            body: JSON.stringify({name,email,phone}),
+          });
+      
+  }
+
+
+    const deleteCustomer=async(id)=>{
+        
+        const updatedCustomer=customer.filter((data)=>{
+            return data._id!==id
+        })
+        setCustomer(updatedCustomer)
+        //api call
+        const response = await fetch(`${host}/customer/deleteCustomer/${id}`, {
+            method: "DELETE", 
+             
+            headers: {
+                "auth-token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ3ZTRlNTYyZThjNWNhZmRmZWY2MzdmIn0sImlhdCI6MTY4NTk5OTE5MH0.tANfZWBhWrVuSXEhNvIAutanlz2LpwO0ZaJrDfN3cj0"
+              
+            },
+            
+          });
+           
+    }
+    
+      return (
+
+    <customerContext.Provider value={{ setCustomerId,setCustomer,customer,deleteCustomer,updateCustomer,addCustomer,getAllcustomers, customerId,updateFormValues,setupdateFormValues}}>
+
+        {props.children}
+    </customerContext.Provider>
+  )
+}
+
+export default ProductState
