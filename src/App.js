c682c3b6dba27f422e63eaec8f89191ae66b3d79
Saqib <splasher733@gmail.com>
2023-09-24 01:45:56 +0500

    viewCustomer page created with table

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import AddProduct from './components/addProduct';
 import ViewProduct from './components/viewProduct';
 import ProductState from './context/productState';
 import UpdateModal from './components/updateModal';
+import ViewCustomer from './components/customer/viewCustomer';
 
 
 
 function App() {
   return (
-    
+  
+  
 <ProductState>
 
     <BrowserRouter>
     <div className='flex'>
     <Sidebar/>
+    <ViewCustomer/>
     <Routes>
     <Route path="/addProduct" element={<AddProduct/>}/>
     <Route path="/viewProduct" element={<ViewProduct/>}/>
