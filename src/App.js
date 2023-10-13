87ea8c276871e8dfb892434724a2cc630a98f784
Saqib <splasher733@gmail.com>
2023-10-13 07:11:13 +0500

    loader updated

--- src/App.js
+++ src/App.js
@@ -1,64 +1,57 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import './App.css';
-import Sidebar from './components/sidebar/sidebar';
-import AddProduct from './components/product/addProduct';
-import ViewProduct from './components/product/viewProduct';
-import ProductState from './context/product/productState';
-import UpdateModal from './components/product/updateModal';
-import ViewCustomer from './components/customer/viewCustomer';
-import CustomerState from './context/customer/customerState';
-import UpdateCustomer from './components/customer/updateCustomer';
-import AddCustomers from './components/customer/addCustomers';
-import ViewCustomerSale from './components/customer/customerSale/viewCustomerSale';
-import CustomerSaleState from './context/customerSale/customerSaleState';
-import AddCustomersSale from './components/customer/customerSale/addCustomerSale';
-import EmployeeState from './context/employees/employeeState';
-import ViewEmployee from './components/employee/viewEmployee';
-import AddEmployees from './components/employee/addEmployee';
-import UpdateEmployee from './components/employee/updateEmployee';
-import ViewAttendance from './components/attendance/viewAttendance';
-import AttendanceState from './context/attendance/attendanceState';
-import AttendanceDatePicker from './components/attendance/attendanceDatePicker';
-import AddAttendance from './components/attendance/addAttendance';
-import AddAttendanceDate from './components/attendance/addAttendanceDate';
-import SignIn from './components/loginSignup/signIn';
-import SignUp from './components/loginSignup/signup';
-import AuthState from './context/auth/authState'
-import Loader from "react-spinners/PacmanLoader";
-
+import React, { useContext, useEffect, useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.css"
+import Sidebar from "./components/sidebar/sidebar"
+import AddProduct from "./components/product/addProduct"
+import ViewProduct from "./components/product/viewProduct"
+import ProductState from "./context/product/productState"
+import UpdateModal from "./components/product/updateModal"
+import ViewCustomer from "./components/customer/viewCustomer"
+import CustomerState from "./context/customer/customerState"
+import UpdateCustomer from "./components/customer/updateCustomer"
+import AddCustomers from "./components/customer/addCustomers"
+import ViewCustomerSale from "./components/customer/customerSale/viewCustomerSale"
+import CustomerSaleState from "./context/customerSale/customerSaleState"
+import AddCustomersSale from "./components/customer/customerSale/addCustomerSale"
+import EmployeeState from "./context/employees/employeeState"
+import ViewEmployee from "./components/employee/viewEmployee"
+import AddEmployees from "./components/employee/addEmployee"
+import UpdateEmployee from "./components/employee/updateEmployee"
+import ViewAttendance from "./components/attendance/viewAttendance"
+import AttendanceState from "./context/attendance/attendanceState"
+import AttendanceDatePicker from "./components/attendance/attendanceDatePicker"
+import AddAttendance from "./components/attendance/addAttendance"
+import AddAttendanceDate from "./components/attendance/addAttendanceDate"
+import SignIn from "./components/loginSignup/signIn"
+import SignUp from "./components/loginSignup/signup"
+import AuthState from "./context/auth/authState"
+import Loader from "react-spinners/PacmanLoader"
+import loadingGif from "./loader.gif"
 
 function App() {
-  
-  var authToken=null
-  
+  var authToken = null
+
   const [authenticated, setauthenticated] = useState(false)
   const [tokenUpdate, settokenUpdate] = useState(false)
   const [loading, setloading] = useState(false)
-  let [color, setColor] = useState("#64748b");
+  let [color, setColor] = useState("#64748b")
 
   useEffect(() => {
     setloading(true)
     setTimeout(() => {
       setloading(false)
-    }, 5000);
+    }, 5000)
   }, [])
-  
-  
+
   useEffect(() => {
-    authToken=localStorage.getItem('authToken');
-      if (authToken !== null) {
-        setauthenticated(true);
-      } else {
-        setauthenticated(false); 
-      }
-  }, [tokenUpdate]);
-  
+    authToken = localStorage.getItem("authToken")
+    if (authToken !== null) {
+      setauthenticated(true)
+    } else {
+      setauthenticated(false)
+    }
+  }, [tokenUpdate])
 
- 
-  
-
- 
   return (
     <AuthState>
       <CustomerState>
@@ -67,40 +60,41 @@ function App() {
             <EmployeeState>
               <AttendanceState>
                 <BrowserRouter>
-                {loading ?<div className='h-screen flex items-center justify-center'>
-                <Loader 
-        color={color}
-        loading={loading}
-        size={15}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-        />
-        </div> 
-                 : <div className='flex'>
-                  {authenticated ? <Sidebar /> : null}
-                    <Routes>
-                      <Route path="/addProduct" element={<AddProduct />} />
-                      <Route path="/viewProduct" element={<ViewProduct />} />
-                      <Route path="/update" element={<UpdateModal />} />
-                      <Route path="/viewCustomers" element={<ViewCustomer />} />
-                      <Route path="/updateCustomers" element={<UpdateCustomer />} />
-                      <Route path="/addCustomers" element={<AddCustomers />} />
-                      <Route path="/AddCustomersSale" element={<AddCustomersSale />} />
-                      <Route path="/viewCustomerSale" element={<ViewCustomerSale />} />
-                      <Route path="/viewEmployee" element={<ViewEmployee />} />
-                      <Route path="/AddEmployee" element={<AddEmployees />} />
-                      <Route path="/UpdateEmployee" element={<UpdateEmployee />} />
-                      <Route path="/viewAttendance" element={<ViewAttendance />} />
-                      <Route path="/attendanceDate" element={<AttendanceDatePicker />} />
-                      <Route path="/addAttendance" element={<AddAttendance />} />
-                      <Route path="/addAttendanceDate" element={<AddAttendanceDate />} />
-                      <Route path="/" element={<SignIn settokenUpdate ={settokenUpdate} />} />
-                      <Route path="/signIn" element={<SignIn settokenUpdate ={settokenUpdate} />} />
-                      <Route path="/sidebar" element={<SignIn />} />
-                      <Route path="/signUp" element={<SignUp />} />
-                    </Routes>
-                  </div>
-                  }
+                  {loading ? (
+                    <div className="flex flex-col justify-center items-center w-screen h-screen">
+                      <img
+                        src={loadingGif}
+                        alt="Loading"
+                        className="w-48 h-36"
+                      />
+                      <span >Loading...</span>
+                    </div>
+                  ) : (
+                    <div className="flex">
+                      {authenticated ? <Sidebar /> : null}
+                      <Routes>
+                        <Route path="/addProduct" element={<AddProduct />} />
+                        <Route path="/viewProduct" element={<ViewProduct />} />
+                        <Route path="/update" element={<UpdateModal />} />
+                        <Route path="/viewCustomers" element={<ViewCustomer />} />
+                        <Route path="/updateCustomers" element={<UpdateCustomer />} />
+                        <Route path="/addCustomers" element={<AddCustomers />} />
+                        <Route path="/AddCustomersSale" element={<AddCustomersSale />} />
+                        <Route path="/viewCustomerSale" element={<ViewCustomerSale />} />
+                        <Route path="/viewEmployee" element={<ViewEmployee />} />
+                        <Route path="/AddEmployee" element={<AddEmployees />} />
+                        <Route path="/UpdateEmployee" element={<UpdateEmployee />} />
+                        <Route path="/viewAttendance" element={<ViewAttendance />} />
+                        <Route path="/attendanceDate" element={<AttendanceDatePicker />} />
+                        <Route path="/addAttendance" element={<AddAttendance />} />
+                        <Route path="/addAttendanceDate" element={<AddAttendanceDate />} />
+                        <Route path="/" element={<SignIn settokenUpdate={settokenUpdate} />} />
+                        <Route path="/signIn" element={<SignIn settokenUpdate={settokenUpdate} />} />
+                        <Route path="/sidebar" element={<SignIn />} />
+                        <Route path="/signUp" element={<SignUp />} />
+                      </Routes>
+                    </div>
+                  )}
                 </BrowserRouter>
               </AttendanceState>
             </EmployeeState>
@@ -108,7 +102,7 @@ function App() {
         </ProductState>
       </CustomerState>
     </AuthState>
-  );
+  )
 }
 
-export default App;
+export default App
