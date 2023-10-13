db153964898349a83861469471a484965ca1bc1a
Saqib <splasher733@gmail.com>
2023-10-13 06:26:14 +0500

    signup validation done and made it responsive

--- src/components/loginSignup/signup.js
+++ src/components/loginSignup/signup.js
@@ -1,89 +1,160 @@
-import React, { useContext } from 'react'
-import authContext from '../../context/auth/authContext'
-
-
-
-
+import React, { useContext, useState, useEffect } from "react"
+import authContext from "../../context/auth/authContext"
+import validator from "validator"
 
 const SignUp = () => {
-const {email,setEmail,password,setPassword,signup,setName,name,responeFromServerSignUp}=useContext(authContext)
+  const { email, setEmail, password, setPassword, signup, setName, name, responeFromServerSignUp } =
+    useContext(authContext)
+  const [validation, setvalidation] = useState("")
+  const [validateEmail, setvalidateEmail] = useState("")
+  const [validatePassword, setvalidatePassword] = useState("")
+  const [validateName, setvalidateName] = useState("")
 
+  const handleChange = (e) => {
+    const { name, value } = e.target
 
-const handleChange = (e) => {
-  const { name, value } = e.target;
-  
-  if (name === 'email') {
-    setEmail(value);
-  } else if (name === 'password') {
-    setPassword(value);
+    if (name === "email") {
+      setEmail(value)
+    } else if (name === "password") {
+      setPassword(value)
+    } else if (name === "name") {
+      setName(value)
+    }
   }
-   else if (name === 'name') {
-    setName(value);
+  const signUp = async () => {
+    try {
+      await signup(email, password, name)
+      return (window.location.href = "/signIn")
+    } catch (error) {
+      console.error("Error during signup:", error)
+    }
   }
-};
 
-const checkSignUp = async () => {
-  try {
-    await signup(email, password, name);
-    return window.location.href = '/signIn';
-  
-  } catch (error) {
-    console.error("Error during signup:", error);
-    
+  const checkSignUp = async () => {
+    if (validator.isEmail(email)) {
+      setvalidateEmail(true)
+    } else {
+      setvalidation("email")
+      setvalidatePassword(false)
+      setvalidateName(false)
+    }
+    if (validator.isLength(password, { min: 8 })) {
+      setvalidatePassword(true)
+    } else {
+      setvalidation("password")
+      setvalidateEmail(false)
+      setvalidateName(false)
+    }
+    if (validator.isLength(name, { min: 1 })) {
+      setvalidateName(true)
+    } else {
+      setvalidation("name")
+      setvalidateEmail(false)
+      setvalidatePassword(false)
+    }
   }
-};
+  useEffect(() => {
+    if (validateEmail && validatePassword && validateName) {
+      signUp()
+    }
+  }, [validateEmail, validatePassword, validateName])
 
-const onClick = (event) => {
-  event.preventDefault();
-  checkSignUp()
-  
- 
- 
-  
+  const onClick = async (event) => {
+    event.preventDefault()
+    await checkSignUp()
+    console.log(validateEmail + " " + validatePassword)
+  }
 
-  
-  
-};
   return (
-    <section className="bg-white min-h-screen flex justify-center items-center ml-96">
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 ml-44">
-      <div className="w-full bg-white rounded-lg shadow-md md:mt-0 sm:max-w-md xl:p-0">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-            Sign up for an account
-          </h1>
-                    <div>
-                        <label  for="name" class="block mb-2 text-sm font-medium text-gray-900">Your Name</label>
-                        <input onChange={handleChange} type="name" name="name" id="name" class="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Saqib" required=""/>
-                    </div>
-          <form className="space-y-4 md:space-y-6" action="#">
-                    <div>
-                        <label  for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                        <input onChange={handleChange} type="email" name="email" id="email" class="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required=""/>
-                    </div>
-                    <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                        <input onChange={handleChange} type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                              <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-100 focus:ring-3 focus:ring-primary-300" required=""/>
-                            </div>
-                            <div class="ml-3 text-sm">
-                              <label for="remember" class="text-gray-500">Remember me</label>
-                            </div>
-                        </div>
-                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
-                    </div>
-                    <button onClick={onClick}  class="w-full text-black bg-slate-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign Up</button>
-                    
-                </form>
+    <div className="flex justify-center items-center w-screen h-screen">
+      <section className="">
+        <div className="max-w-md w-full">
+          <div className="bg-slate-300 rounded-lg shadow-md p-6 md:p-8">
+            <h1 className="text-xl md:text-2xl font-bold leading-tight tracking-tight text-gray-900 text-center">
+              Sign up for an account
+            </h1>
+            <div className="mb-4">
+              {validation === "email" ? (
+                <span className="text-red-700 block mb-2 text-sm font-medium">Enter correct email!</span>
+              ) : validation === "password" ? (
+                <span className="text-red-700 block mb-2 text-sm font-medium">Password minimum 8 characters!</span>
+              ) : validation === "name" ? (
+                <span className="text-red-700 block mb-2 text-sm font-medium">Name cannot be empty!</span>
+              ) : (
+                ""
+              )}
+              <label for="name" className="block mb-2 text-sm font-medium text-gray-900">
+                Your Name
+              </label>
+              <input
+                onChange={handleChange}
+                type="name"
+                name="name"
+                id="name"
+                className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                placeholder="Saqib"
+                required=""
+              />
             </div>
+            <form className="space-y-4" action="#">
+              <div className="mb-4">
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900">
+                  Your email
+                </label>
+                <input
+                  onChange={handleChange}
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="name@company.com"
+                  required=""
+                />
+              </div>
+              <div className="mb-4">
+                <label for="password" className="block mb-2 text-sm font-medium text-gray-900">
+                  Password
+                </label>
+                <input
+                  onChange={handleChange}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required=""
+                />
+              </div>
+              <div className="mb-4 flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-100 focus:ring-3 focus:ring-primary-300"
+                      required=""
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label className="text-gray-500">Remember me</label>
+                  </div>
+                </div>
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline">
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                onClick={onClick}
+                className="w-full text-black bg-slate-400 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
         </div>
+      </section>
     </div>
-</section>
-
   )
 }
 
