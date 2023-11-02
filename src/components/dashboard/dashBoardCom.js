cdc062fd5daecdd684db9fc1ae1fd388caf4691a
Saqib <splasher733@gmail.com>
2023-11-03 04:27:26 +0500

    Dashboard created

---
+++ src/components/dashboard/dashBoardCom.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Barchart from './barchart'
+import Arreachart from './areaChart'
+
+const Dashboard = () => {
+  return (
+    <>
+    <div className='  pl-60 py-24'><Barchart/></div>
+    <div>
+      
+    </div>
+    < Arreachart/>
+    </>
+  )
+}
+
+export default Dashboard
