70ab8846bda313ee68ceafab08053954f64ac2ba
Saqib <splasher733@gmail.com>
2023-11-10 02:49:54 +0500

    validated fields

--- src/components/customer/customerSale/addCustomerSale.js
+++ src/components/customer/customerSale/addCustomerSale.js
@@ -1,32 +1,85 @@
-import React, { useContext, useState } from 'react'
-import customerSaleContext from "../../../context/customerSale/customerSaleContext";
+import React, { useContext, useState } from 'react';
+import customerSaleContext from '../../../context/customerSale/customerSaleContext';
 import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import validator from 'validator';
+import { useEffect } from 'react';
+
 const AddCustomerSale = () => {
+  const Navigate = useNavigate();
+  const { addCustomerSale } = useContext(customerSaleContext);
+  const [customerSale, setcustomerSale] = useState({
+    customerId: '',
+    product: '',
+    quantity: '',
+  });
+  const [CustomerIdValid, setCustomerIdValid] = useState(false);
+  const [ProductValid, setProductValid] = useState(false);
+  const [QuantityValid, setQuantityValid] = useState(false);
+
+  const handleChange = (e) => {
+    setcustomerSale({ ...customerSale, [e.target.name]: e.target.value });
+  };
+  
+  const NameValidator = (str) => {
+    return /^[A-Za-z0-9\s]+$/.test(str);
+  }
+
+  const onClick = (event) => {
+    event.preventDefault();
+
+    if (
+      customerSale.customerId === '' ||
+      customerSale.product === '' ||
+      customerSale.quantity === ''
+    ) {
+      return toast.error('Please Enter All Fields');
+    }
+
+    if (validator.isAlphanumeric(customerSale.customerId)) {
+      setCustomerIdValid(true);
+    } else {
+      toast.error('Enter Valid Customer Id');
+      setCustomerIdValid(false);
+    }
+
     
-    const Navigate = useNavigate();
-    const {addCustomerSale}=useContext(customerSaleContext)
-    const [customerSale, setcustomerSale] = useState({customerId:"",product:"",quantity:""})
-    
-    const handleChange=(e)=>{
-       setcustomerSale({...customerSale,[e.target.name]:e.target.value})
-      }
-      
-      const onClick=(event)=>{
-        event.preventDefault();
-        addCustomerSale(customerSale.customerId,customerSale.product,customerSale.quantity)
-        Navigate('/viewCustomerSale')    
-    
-      }
+    if (NameValidator(customerSale.product)) {
+      setProductValid(true);
+    } else {
+      toast.error('Enter Valid Product');
+      setProductValid(false);
+    }
+
+    if (validator.isNumeric(customerSale.quantity)) {
+      setQuantityValid(true);
+    } else {
+      toast.error('Enter Valid Quantity');
+      setQuantityValid(false);
+    }
+
+  };
+  useEffect(() => {
+    if (CustomerIdValid && ProductValid && QuantityValid) {
+      addCustomerSale(
+        customerSale.customerId,
+        customerSale.product,
+        customerSale.quantity
+      );
+      Navigate('/viewCustomerSale');
+    }
+  }, [CustomerIdValid ,ProductValid ,QuantityValid])
+  
   return (
-    <div className="shadow-xl w-full lg:ml-96 mr-52 my-20 md:ml-96 mr-52 my-20 sm:ml-96 mr-52 my-20  ">
+    <div className="lg:mx-auto sm:ml-64 sm:items-end  shadow-xl h-1/2 mx-auto my-20 border bg-slate-50 border-gray-300 rounded-xl">
     <div className="pl-8 py-8 px-8  pr-8">
       <div>
         <h1 class="mb-1 font-bold text-3xl flex gap-1 items-baseline font-mono">
           Add Customer Sale<span class="text-sm text-gray-400">SAS ERP</span>
         </h1>
-        <div class="grid max-w-3xl gap-5 py-10 px-8 sm:grid-cols-1 bg-white rounded-md border-t-4 border-gray-400">
-          <div class="grid">
-            <div class="bg-white flex min-h-[60px] flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:shadow-inner">
+        <div className="grid max-w-3xl lg:gap-8 sm:gap-3 py-10 px-8 sm:grid-cols-1 bg-slate-50 rounded-md border-t-4 border-gray-400">
+            <div className="grid">
+              <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 ">
               <input
                 type="text"
                 name="quantity"
@@ -44,7 +97,7 @@ const AddCustomerSale = () => {
           </div>
          
           <div class="grid">
-            <div class="bg-white flex min-h-[60px] flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:shadow-inner">
+          <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 ">
               <input
                 type="text"
                 name="customerId"
@@ -61,7 +114,7 @@ const AddCustomerSale = () => {
             </div>
           </div>
           <div class="grid">
-            <div class="bg-white flex min-h-[60px] flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:shadow-inner">
+          <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 ">
               <input
                 
                 name="product"
