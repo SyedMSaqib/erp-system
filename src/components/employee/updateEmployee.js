4b1395e70d3b3f68ce41874d6994c7c1101076a2
Saqib <splasher733@gmail.com>
2023-11-14 04:55:21 +0500

    Theme Updated

--- src/components/employee/updateEmployee.js
+++ src/components/employee/updateEmployee.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react';
 import EmployeeContext from '../../context/employees/employeeContext';
 import { useNavigate } from 'react-router-dom';
-import toast from "react-hot-toast";
-import validator from "validator";
+import toast from 'react-hot-toast';
+import validator from 'validator';
 
 const UpdateEmployee = () => {
   const navigate = useNavigate();
@@ -13,10 +13,10 @@ const UpdateEmployee = () => {
   const [emailValid, setEmailValid] = useState(false);
   const [phoneValid, setPhoneValid] = useState(false);
 
-  
   const NameValidator = (str) => {
     return /^[A-Za-z\s]+$/.test(str);
-  }
+  };
+
   useEffect(() => {
     if (updateFormValues) {
       setEmployee(updateFormValues);
@@ -30,32 +30,30 @@ const UpdateEmployee = () => {
   const onClick = (event) => {
     event.preventDefault();
 
-    
-    if (employee.name === "" || employee.email === "" || employee.phone === "") {
-      return toast.error("Please Enter All Fields");
+    if (employee.name === '' || employee.email === '' || employee.phone === '') {
+      return toast.error('Please Enter All Fields');
     }
 
     if (NameValidator(employee.name)) {
       setNameValid(true);
     } else {
-      toast.error("Enter Valid Name");
+      toast.error('Enter Valid Name');
       setNameValid(false);
     }
 
     if (validator.isEmail(employee.email)) {
       setEmailValid(true);
     } else {
-      toast.error("Enter Valid Email");
+      toast.error('Enter Valid Email');
       setEmailValid(false);
     }
 
     if (validator.isNumeric(employee.phone)) {
       setPhoneValid(true);
     } else {
-      toast.error("Enter Valid Phone Number");
+      toast.error('Enter Valid Phone Number');
       setPhoneValid(false);
     }
-    
   };
 
   useEffect(() => {
@@ -68,72 +66,81 @@ const UpdateEmployee = () => {
   }, [nameValid, emailValid, phoneValid]);
 
   return (
-    <div className="flex justify-center items-center w-screen " >
-    <div className="shadow-xl w-full lg:ml-96 mr-52 my-20 md:ml-96 mr-52 my-20 sm:ml-96 mr-52 my-20">
-      <div className="lg:pl-20 border-2 rounded-lg pr-28 md:pl-20 pr-28 sm:pl-20 pr-28">
-        <h1 className="font-semibold py-8">Update Employee</h1>
-        <form className="pt-10">
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                Name
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
-                placeholder=""
-                name="name"
-                onChange={handleChange}
-                value={employee.name || ''}
-              />
-            </div>
-          </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                Email
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
-                placeholder=""
-                name="email"
-                onChange={handleChange}
-                value={employee.email || ''}
-              />
-            </div>
-          </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                Phone
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
-                placeholder=""
-                name="phone"
-                onChange={handleChange}
-                value={employee.phone || ''}
-              />
-              <div className="flex justify-center  " >
-                <button
-                  onClick={onClick}
-                  className="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group"
-                >
-                  <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-                  <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-                  <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-                  <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-                  <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-                  <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Submit</span>
-                </button>
+    <div className="lg:mx-auto sm:ml-64 sm:items-end  shadow-xl h-1/2 mx-auto my-20 border bg-slate-50 border-gray-300 rounded-xl">
+      <div className="pl-8 py-8 px-8  pr-8">
+        <div className="">
+          <div className="bg-slate-50 ">
+            <h1 className="mb-1 font-bold pr-24 text-3xl flex gap-1 items-baseline font-mono">
+              Update Employee<span className="text-sm text-gray-400">SAS ERP</span>
+            </h1>
+            <div className="grid max-w-3xl lg:gap-8 sm:gap-3 py-10 px-8 sm:grid-cols-1 bg-slate-50 rounded-md border-t-4 border-gray-400">
+            <div className="grid">
+            <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 ">
+                  <input
+                    name="name"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Name"
+                    onChange={handleChange}
+                    value={employee.name || ''}
+                  />
+                  <label
+                    htmlFor="name"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    Name
+                  </label>
+                </div>
               </div>
+
+              <div className="grid">
+            <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 ">
+                  <input
+                    type="text"
+                    name="phone"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Phone"
+                    onChange={handleChange}
+                    value={employee.phone || ''}
+                  />
+                  <label
+                    htmlFor="company"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    Phone No
+                  </label>
+                </div>
+              </div>
+
+              <div className="grid">
+                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 ">
+                  <input
+                    type="text"
+                    name="email"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Email"
+                    onChange={handleChange}
+                    value={employee.email || ''}
+                  />
+                  <label
+                    htmlFor="email"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    E-mail
+                  </label>
+                </div>
+              </div>
+
+              <button
+                onClick={onClick}
+                type="submit"
+                className="mt-4 bg-slate-400 text-white py-2 px-6 rounded-md hover:bg-slate-500"
+              >
+                Submit
+              </button>
             </div>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
