8f623477b805c893b8a9b3270025e3e7dbe4d0ab
Saqib <splasher733@gmail.com>
2023-11-14 04:55:29 +0500

    Theme Updated

--- src/components/product/addProduct.js
+++ src/components/product/addProduct.js
@@ -1,147 +1,193 @@
-import React, { useContext, useEffect, useState } from 'react'
-import ProductContext from '../../context/product/productContext'
+import React, { useContext, useEffect, useState } from 'react';
+import ProductContext from '../../context/product/productContext';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 import validator from 'validator';
 
-
-
 const AddProduct = () => {
-  
-  const Navigate = useNavigate();
-  const productContext=useContext(ProductContext)
-  const{addProduct}=productContext
-  const [product, setproduct] = useState({name:"",category:"",description:"",quantity:"",price:""})
- const [ProductName, setProductName] = useState(false)
- const [ProductPrice, setProductPrice] = useState(false)
- const [ProductQuantity, setProductQuantity] = useState(false)
- const NameValidator=(str)=> {
-  return /^[A-Za-z\s]+$/.test(str);
-}
-  const handleChange=(e)=>{
-    setproduct({...product,[e.target.name]:e.target.value})
-  }
-  const onClick=(event)=>{
+  const NameValidator = (str) => {
+    return /^[A-Za-z\s]+$/.test(str);
+  };
+
+  const navigate = useNavigate();
+  const productContext = useContext(ProductContext);
+  const { addProduct } = productContext;
+  const [product, setProduct] = useState({
+    name: "",
+    category: "",
+    description: "",
+    quantity: "",
+    price: ""
+  });
+  const [isValidName, setIsValidName] = useState(false);
+  const [isValidPrice, setIsValidPrice] = useState(false);
+  const [isValidQuantity, setIsValidQuantity] = useState(false);
+
+  const handleChange = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  };
+
+  const onClick = (event) => {
     event.preventDefault();
-    
-    if(product.category===""||product.description==="")
-    return toast.error("Please Enter All Fields")
 
-    if(NameValidator(product.name))
-    {
-      setProductName(true)
+    if (product.category === "" || product.description === "") {
+      return toast.error("Please Enter All Fields");
     }
-    else
-  { 
-    toast.error("Enter Valid Name")
-     setProductPrice(false)
-    setProductQuantity(false)
+
+    if (NameValidator(product.name)) {
+      setIsValidName(true);
+    } else {
+      toast.error("Enter Valid Name");
+      setIsValidName(false);
+      
     }
-    if(validator.isNumeric(product.quantity))
-    {
-      setProductQuantity(true)
+
+    if (validator.isNumeric(product.quantity)) {
+      setIsValidQuantity(true);
+    } else {
+      toast.error("Enter Valid Quantity");
+      setIsValidQuantity(false);
     }
-    else
-    {
-    toast.error("Enter Valid Quantity")
-      setProductName(false)
-      setProductPrice(false)
+
+    if (validator.isNumeric(product.price)) {
+      setIsValidPrice(true);
+    } else {
+      toast.error("Enter Valid Price");
+      setIsValidPrice(false);
     }
-    if(validator.isNumeric(product.price))
-    {
-      setProductPrice(true)
+  };
+
+  useEffect(() => {
+    if (isValidName && isValidQuantity && isValidPrice) {
+      addProduct(
+        product.name,
+        product.description,
+        product.category,
+        product.quantity,
+        product.price
+      );
+      toast.success(`${product.name} added to Inventory`);
+      navigate('/viewProduct');
     }
-    else
-    {
-    toast.error("Enter Valid Price")
-      setProductName(false)
-      setProductQuantity(false)
-    }
-  }
- 
-    useEffect(() => {
-      if(ProductName&&ProductPrice&&ProductQuantity)
-      {
-        AddProductToDb()
-      }
-    }, [ProductName,ProductPrice,ProductQuantity])
-    
-    const AddProductToDb=()=>{
-      addProduct(product.name,product.description,product.category,product.quantity,product.price)
-      toast.success(`${product.name} added to Inventory`)
-      Navigate('/viewProduct');
-    }
-  
-  
-  
-  
+  }, [isValidName, isValidQuantity, isValidPrice, navigate, product, addProduct]);
+
   return (
-    <div className="flex justify-center items-center w-screen h-screen mt-28" >
-    <div className="  shadow-xl w-full ml-96 mr-52 my-20   ">
-      <div className='pl-20 border-2 rounded-lg pr-28 '>
+    <div className="lg:mx-auto sm:ml-64 sm:items-end shadow-xl h-1/2 mt-3 mb-5  mx-auto  border bg-slate-50 border-gray-300 rounded-xl">
+      <div className="pl-8 py-8 px-8 pr-8">
+        <div className="">
+          <div className="bg-slate-50">
+            <h1 className="mb-1 font-bold pr-24 text-3xl flex gap-1 items-baseline font-mono">
+              Add Product<span className="text-sm text-gray-400">SAS ERP</span>
+            </h1>
+            <div className="grid max-w-3xl lg:gap-8 sm:gap-3 py-10 px-8 sm:grid-cols-1 bg-slate-50 rounded-md border-t-4 border-gray-400">
+         
+              <div className="grid">
+                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
+                  <input
+                    name="name"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Name"
+                    onChange={handleChange}
+                  />
+                  <label
+                    htmlFor="name"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    Name
+                  </label>
+                </div>
+              </div>
 
-      
-    <h1 className='font-semibold py-8'>Add Product</h1>
-<form className='pt-10'>
-    
-  
-  <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full px-3">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-        Name
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="description" placeholder="" name='name' onChange={handleChange}/>
-    
-    </div>
-  </div>
-  <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full px-3">
-      
-      <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full px-3">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-        Category
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="description" placeholder="" minLength={5} name='category' onChange={handleChange}/>
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-        Price
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="description" placeholder="" minLength={5} name='price' onChange={handleChange}/>
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-        Quantity
-      </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="description" placeholder="" minLength={5} name='quantity' onChange={handleChange}/>
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-        Description
-      </label>
-      <input className=" h-40 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="description" placeholder="" name='description' onChange={handleChange}/>
-    
-    </div>
-  </div>
+           
+              <div className="grid">
+                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
+                  <input
+                    type="text"
+                    name="category"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Category"
+                    onChange={handleChange}
+                  />
+                  <label
+                    htmlFor="category"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    Category
+                  </label>
+                </div>
+              </div>
 
-  
-  <div className="flex justify-center  " >
+              
+              <div className="grid">
+                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
+                  <input
+                    type="text"
+                    name="price"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Price"
+                    onChange={handleChange}
+                  />
+                  <label
+                    htmlFor="price"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    Price
+                  </label>
+                </div>
+              </div>
 
+              {/* Quantity Input */}
+              <div className="grid">
+                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
+                  <input
+                    type="text"
+                    name="quantity"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Quantity"
+                    onChange={handleChange}
+                  />
+                  <label
+                    htmlFor="quantity"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    Quantity
+                  </label>
+                </div>
+              </div>
 
-  <button onClick={onClick}  class="relative px-5 py-3 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group">
-<span class="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
-<span class="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
-<span class="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-<span class="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-gray-600 group-hover:h-full ease"></span>
-<span class="absolute inset-0 w-full h-full duration-300 delay-300 bg-gray-900 opacity-0 group-hover:opacity-100"></span>
-<span class="relative transition-colors duration-300 delay-200 group-hover:text-white ease">Submit</span>
-</button>
+              {/* Description Input */}
+              <div className="grid">
+                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
+                  <input
+                    type="text"
+                    name="description"
+                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
+                    placeholder="Description"
+                    onChange={handleChange}
+                  />
+                  <label
+                    htmlFor="description"
+                    className="block transform text-xs font-bold uppercase text-gray-400 transition-opacity duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0"
+                  >
+                    Description
+                  </label>
+                </div>
+              </div>
+
+              {/* Submit Button */}
+              <button
+                onClick={onClick}
+                type="submit"
+                className="mt-4 bg-slate-400 text-white py-2 px-6 rounded-md hover:bg-slate-500"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    
     </div>
-  </div>
-</form>
-</div>
-  </div>
-  </div>
+  );
+};
 
-
-  )
-}
-
-export default AddProduct
+export default AddProduct;
