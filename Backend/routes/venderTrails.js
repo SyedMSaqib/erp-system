31f76d74b98daf248a33c15b07fb2ccf15b3c42a
Saqib <splasher733@gmail.com>
2023-11-25 14:05:04 +0500

    vender trail route created

---
+++ Backend/routes/venderTrails.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const validator = require("../middleware/validator");
+const venderTrails = require("../models/venderTrails");
+
+router.put(
+  "/addVenderTrail/:id",
+  validator,
+  async (req, res) => {
+    const id = req.params.id;
+
+    try {
+      if (req.user == null) return res.status(404).send("Invalid token, or empty");
+      const updatedVenderTrails = {
+        paid: true,
+      };
+      await venderTrails.findByIdAndUpdate(id, { $set: updatedVenderTrails }, { new: true });
+      res.status(200).send({ status: 200, msg: "Updated" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+);
+
+router.get("/fetchAllVenderTrails", validator, async (req, res) => {
+  try {
+    const VenderTrails = await venderTrails.find({ user: req.user.id });
+    
+    res.status(200).json({ venderTrails: VenderTrails, status: 200 });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
