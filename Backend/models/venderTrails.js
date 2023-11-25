dc85358326af1f7a00300b627676f4e1b5140059
Saqib <splasher733@gmail.com>
2023-11-25 14:04:46 +0500

    venderTrailsSchema created

---
+++ Backend/models/venderTrails.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const VenderTrailSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+
+  venderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "vender",
+    required: true,
+  },
+  venderName: {
+    type: String,
+    ref: "vender",
+    required: true,
+  },
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "product",
+    required: true,
+  },
+  paid: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+  purchaseAmount: {
+    type: Number,
+    required: true,
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+module.exports = mongoose.model("VenderTrail", VenderTrailSchema)
