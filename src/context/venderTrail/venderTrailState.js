fd68e12c6f6bac0db31c55ab07838e03a5e12a08
Saqib <splasher733@gmail.com>
2023-11-25 14:06:21 +0500

    vender trail state added

---
+++ src/context/venderTrail/venderTrailState.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import VenderTrailContext from "./venderTrailContext"; 
+import config from "../../config";
+
+const VenderTrailState = (props) => {
+  const host = config.apiurl;
+  const authToken = localStorage.getItem("authToken");
+  const [venderTrails, setVenderTrails] = useState();
+
+  const getAllVenderTrails = async () => {
+    try {
+      const response = await fetch(`${host}/venderTrail/fetchAllVenderTrails`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": authToken,
+        },
+      });
+
+      const data = await response.json();
+      setVenderTrails(data);
+    } catch (error) {
+      console.error("Error fetching vender trails:", error);
+    }
+  };
+
+  const updateVenderTrail = async (id) => {
+    try {
+      await fetch(`${host}/venderTrail/addVenderTrail/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": authToken,
+        },
+      });
+    } catch (error) {
+      console.error("Error updating vender trail:", error);
+    }
+  };
+
+  return (
+    <VenderTrailContext.Provider
+      value={{
+        venderTrails,
+        getAllVenderTrails,
+        updateVenderTrail,
+      }}
+    >
+      {props.children}
+    </VenderTrailContext.Provider>
+  );
+};
+
+export default VenderTrailState;
