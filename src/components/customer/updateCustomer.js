858687ad2fc4bbb287dfc8684f33632d85e09986
Saqib <splasher733@gmail.com>
2023-11-29 03:13:42 +0500

    styling errors fixed

--- src/components/customer/updateCustomer.js
+++ src/components/customer/updateCustomer.js
@@ -89,19 +89,19 @@ const UpdateCustomer = () => {
 
   return (
     <div className='lg:flex lg:justify-center lg:content-center sm:ml-64'>
-    <div className="shadow-xl md:w-[30rem] sm:w-[20rem] mb-5 lg:mt-2  mx-auto  border bg-slate-50 border-gray-300 rounded-xl">
-      <div className="pl-8 py-8 px-8 pr-8">
-        <div className="">
-          <div className="bg-slate-50">
+    <div className="shadow-xl md:w-[30rem] sm:w-[20rem] mb-5 lg:mt-10 mx-auto  border dark:bg-gray-950 bg-slate-50 dark:border-gray-600 border-gray-300 rounded-xl">
+    <div className="pl-8 py-8 px-8  pr-8">
+      <div className="">
+        <div className="bg-slate-50 dark:bg-gray-950  dark:text-gray-300">
             <h1 className="mb-1 font-bold pr-24 text-3xl flex gap-1 items-baseline font-mono">
-              Update Customer<span className="text-sm text-gray-400">SAS ERP</span>
+              Update Customer<span className="text-sm text-gray-400 ">SAS ERP</span>
             </h1>
-            <div className="grid max-w-3xl lg:gap-8 sm:gap-3 py-10 px-8 sm:grid-cols-1 bg-slate-50 rounded-md border-t-4 border-gray-400">
+            <div className="grid max-w-3xl lg:gap-8 sm:gap-3 py-10 px-8 sm:grid-cols-1 bg-slate-50 rounded-md border-t-4 border-gray-400  dark:bg-gray-950">
               <div className="grid">
-                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
-                  <input
+              <div  className="bg-white dark:bg-gray-900 flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 dark:border-gray-700">
+                      <input
+                        className="peer dark:bg-gray-900 block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0 dark:text-gray-300"
                     name="name"
-                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
                     placeholder="Name"
                     onChange={handleChange}
                     value={customer.name || ''}
@@ -116,11 +116,11 @@ const UpdateCustomer = () => {
               </div>
 
               <div className="grid">
-                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
-                  <input
+              <div  className="bg-white dark:bg-gray-900 flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 dark:border-gray-700">
+                      <input
+                        className="peer dark:bg-gray-900 block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0 dark:text-gray-300"
                     type="text"
                     name="phone"
-                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
                     placeholder="Phone"
                     onChange={handleChange}
                     value={customer.phone || ''}
@@ -135,11 +135,11 @@ const UpdateCustomer = () => {
               </div>
 
               <div className="grid">
-                <div className="bg-white flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2">
-                  <input
+              <div  className="bg-white dark:bg-gray-900 flex flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 dark:border-gray-700">
+                      <input
+                        className="peer dark:bg-gray-900 block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0 dark:text-gray-300"
                     type="text"
                     name="email"
-                    className="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0"
                     placeholder="Email"
                     onChange={handleChange}
                     value={customer.email || ''}
