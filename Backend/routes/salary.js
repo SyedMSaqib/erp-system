69c98b1db7df1dc5b5e1e8bc8d31219d896d8910
Saqib <splasher733@gmail.com>
2023-11-29 01:28:03 +0500

    view salary record added

--- Backend/routes/salary.js
+++ Backend/routes/salary.js
@@ -1,36 +1,31 @@
-const express = require("express");
-const router = express.Router();
-const validator = require("../middleware/validator");
-const Salary = require("../models/salary");
-const { check, validationResult } = require("express-validator");
-const employee= require("../models/employee");
-const salary = require("../models/salary");
+const express = require("express")
+const router = express.Router()
+const validator = require("../middleware/validator")
+const Salary = require("../models/salary")
+const { check, validationResult } = require("express-validator")
+const employee = require("../models/employee")
+const salary = require("../models/salary")
 // Add Salary
 router.post(
   "/addSalary",
-  [
-    check("Month").isLength({ min: 1 }),
-    check("days").isNumeric(),
-    
-  ],
+  [check("Month").isLength({ min: 1 }), check("days").isNumeric()],
   validator,
   async (req, res) => {
-    const result = validationResult(req);
-    if (!result.isEmpty()) return res.json(result);
+    const result = validationResult(req)
+    if (!result.isEmpty()) return res.json(result)
 
     try {
-      if (req.user == null) return res.status(404).send("Invalid token or empty");
-      const {Month,days } = req.body;
-      const employees=await employee.find({user:req.user.id})
-      const checkAlreadyMonth=await salary.findOne({Month:Month})
-      
-      if(checkAlreadyMonth)
-      return res.status(400).send("Already Paid this month")
-      
-      for (const emp of employees) {
-        const { name, basePay,_id } = emp;
+      if (req.user == null) return res.status(404).send("Invalid token or empty")
+      const { Month, days } = req.body
+      const employees = await employee.find({ user: req.user.id })
+      const checkAlreadyMonth = await salary.findOne({ Month: Month })
 
-        const monthlyPay = basePay * days;
+      if (checkAlreadyMonth) return res.status(400).send("Already Paid this month")
+
+      for (const emp of employees) {
+        const { name, basePay, _id } = emp
+
+        const monthlyPay = basePay * days
 
         await Salary.create({
           user: req.user.id,
@@ -39,71 +34,90 @@ router.post(
           Month: Month,
           basePay: basePay,
           monthlyPay: monthlyPay,
-        });
+        })
       }
-      return res.status(202).json({ message: "Salaries created successfully" });
+      return res.status(202).json({ message: "Salaries created successfully" })
     } catch (err) {
-      return res.status(500).json(err);
+      return res.status(500).json(err)
     }
   }
-);
+)
 
-// Fetch All Salaries
+router.post("/monthRecord", [check("Month").isLength({ min: 1 })], validator, async (req, res) => {
+  const result = validationResult(req)
+  if (!result.isEmpty()) return res.json(result)
+  try {
+    if (req.user == null) return res.status(404).send("Invalid token or empty")
+  
+    const {Month} = req.body;
+
+    const salariesRecord = await Salary.find({ user: req.user.id, Month:Month })
+    if(salariesRecord.length!==0){
+      res.json({salariesRecord, status:200})
+    }
+    else
+    res.json({message:"No record available for this month",status:400})
+  } catch (err) {
+    res.json(err)
+  }
+})
 router.get("/fetchAllSalaries", validator, async (req, res) => {
   try {
-    const salariesFromDb = await Salary.find({ user: req.user.id });
-    res.json(salariesFromDb);
+    const { Month, days } = req.body
+
+    const salariesFromDb = await Salary.find({ user: req.user.id })
+    res.json(salariesFromDb)
   } catch (err) {
-    res.json(err);
+    res.json(err)
   }
-});
+})
 
 // Delete Salary
 router.delete("/deleteSalary/:id", validator, async (req, res) => {
-  const id = req.params.id;
-  if (id === null) return res.status(500).send("Input correct id");
+  const id = req.params.id
+  if (id === null) return res.status(500).send("Input correct id")
 
   try {
-    const salaryFromDb = await Salary.findById(id);
-    if (!salaryFromDb) return res.status(404).json("No such salary exists");
+    const salaryFromDb = await Salary.findById(id)
+    if (!salaryFromDb) return res.status(404).json("No such salary exists")
 
-    if (salaryFromDb.user.toString() !== req.user.id) return res.status(404).send("Unauthorized user");
-    const salaryDelete = await Salary.findByIdAndDelete(id);
+    if (salaryFromDb.user.toString() !== req.user.id) return res.status(404).send("Unauthorized user")
+    const salaryDelete = await Salary.findByIdAndDelete(id)
 
-    res.json(salaryDelete);
+    res.json(salaryDelete)
   } catch (err) {
-    res.json(`${err}`);
+    res.json(`${err}`)
   }
-});
+})
 
 // Update Salary
 router.put("/updateSalary/:id", validator, async (req, res) => {
-  const result = validationResult(req);
-  if (!result.isEmpty()) return res.json(result);
+  const result = validationResult(req)
+  if (!result.isEmpty()) return res.json(result)
 
-  const id = req.params.id;
-  if (id === null) return res.status(500).send("Input correct id");
+  const id = req.params.id
+  if (id === null) return res.status(500).send("Input correct id")
 
   try {
-    const salaryFromDb = await Salary.findById(id);
-    if (!salaryFromDb) return res.status(404).json("No such salary exists");
+    const salaryFromDb = await Salary.findById(id)
+    if (!salaryFromDb) return res.status(404).json("No such salary exists")
 
-    if (salaryFromDb.user.toString() !== req.user.id) return res.status(404).send("Unauthorized user");
-    const { employeeName, employeeId, Month, basePay, monthlyPay } = req.body;
+    if (salaryFromDb.user.toString() !== req.user.id) return res.status(404).send("Unauthorized user")
+    const { employeeName, employeeId, Month, basePay, monthlyPay } = req.body
 
-    const newSalary = {};
-    if (employeeName) newSalary.employeeName = employeeName;
-    if (employeeId) newSalary.employeeId = employeeId;
-    if (Month) newSalary.Month = Month;
-    if (basePay) newSalary.basePay = basePay;
-    if (monthlyPay) newSalary.monthlyPay = monthlyPay;
+    const newSalary = {}
+    if (employeeName) newSalary.employeeName = employeeName
+    if (employeeId) newSalary.employeeId = employeeId
+    if (Month) newSalary.Month = Month
+    if (basePay) newSalary.basePay = basePay
+    if (monthlyPay) newSalary.monthlyPay = monthlyPay
 
-    const salaryUpdate = await Salary.findByIdAndUpdate(id, { $set: newSalary }, { new: true });
+    const salaryUpdate = await Salary.findByIdAndUpdate(id, { $set: newSalary }, { new: true })
 
-    res.json(salaryUpdate);
+    res.json(salaryUpdate)
   } catch (err) {
-    res.json(`${err}`);
+    res.json(`${err}`)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
