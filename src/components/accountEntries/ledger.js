36cc8c576b295b21c0592691afc97e4f53517039
Saqib <splasher733@gmail.com>
2023-12-08 04:26:49 +0500

    ledger added

---
+++ src/components/accountEntries/ledger.js
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect } from "react";
+import EntriesContext from "../../context/accountEntries/EntriesContext";
+
+const Ledger = () => {
+  const { ledger, getAllLedger } = useContext(EntriesContext);
+  const formatMongoDate = (mongoDate) => {
+    const dateObject = new Date(mongoDate)
+
+    const year = dateObject.getFullYear()
+    const month = (dateObject.getMonth() + 1).toString().padStart(2, "0")
+    const day = dateObject.getDate().toString().padStart(2, "0")
+    const hours = dateObject.getHours().toString().padStart(2, "0")
+    const minutes = dateObject.getMinutes().toString().padStart(2, "0")
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`
+  }
+
+  useEffect(() => {
+    getAllLedger();
+  }, []);
+  console.log(ledger)
+  return (
+    <div className="dark:bg-gray-900">
+    <div className="flex justify-center items-center  ">
+      <div className="absolute top-0 text-center ml-52 mt-2 font-semibold text-lg dark:text-gray-300">General Ledger</div>
+      <div className="overflow-auto shadow-lg   m-5 mt-10 ml-64 ">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:border-gray-700 dark:text-gray-400 border border-gray-300">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Journal Entry
+            </th>
+            <th scope="col" className="px-20 py-3">
+              Description
+            </th>
+            <th scope="col" className="px-20 py-3">
+              Amount
+            </th>
+            
+            <th scope="col" className="px-20 py-3">
+              Date
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {ledger.map((entry) => (
+            <tr key={entry._id} className={`${entry.journalEntry==="rr"?"bg-red-200 border-b dark:text-gray-600 text-gray-600 border-gray-300":entry.journalEntry==="dr"?"bg-green-200 border-b dark:text-gray-600 text-gray-600 border-gray-300":entry.journalEntry==="cr"?"bg-yellow-200 border-b dark:text-gray-600 text-gray-600 border-gray-300":""}`}>
+              <td className="px-6 py-4">{(entry.journalEntry).toUpperCase()}</td>
+              <td className="px-20 py-4">{entry.Description}</td>
+              <td className="px-20 py-4">{Math.abs(entry.amount)}</td>
+              <td className="px-20 py-4">{formatMongoDate(entry.date)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+    </div>
+    </div>
+  );
+};
+
+export default Ledger;
