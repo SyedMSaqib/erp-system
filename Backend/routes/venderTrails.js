0962ca5da76ec748533aaaf548ec971682f30120
Saqib <splasher733@gmail.com>
2023-12-08 06:25:12 +0500

    ...

--- Backend/routes/venderTrails.js
+++ Backend/routes/venderTrails.js
@@ -1,52 +1,49 @@
-const express = require("express");
-const router = express.Router();
-const validator = require("../middleware/validator");
-const venderTrails = require("../models/venderTrails");
-const payable = require("../models/payable");
-const ledger = require("../models/ledger");
+const express = require("express")
+const router = express.Router()
+const validator = require("../middleware/validator")
+const venderTrails = require("../models/venderTrails")
+const payable = require("../models/payable")
+const ledger = require("../models/ledger")
 
-router.put(
-  "/addVenderTrail/:id",
-  validator,
-  async (req, res) => {
-    const id = req.params.id;
+router.put("/addVenderTrail/:id", validator, async (req, res) => {
+  const id = req.params.id
 
-    try {
-      if (req.user == null) return res.status(404).send("Invalid token, or empty");
-      const updatedVenderTrails = {
-        paid: true,
-        date: new Date()
-      };
-     const venderDetials= await venderTrails.findByIdAndUpdate(id, { $set: updatedVenderTrails }, { new: true });
-     await payable.create({
+  try {
+    if (req.user == null) return res.status(404).send("Invalid token, or empty")
+    const updatedVenderTrails = {
+      paid: true,
+      date: new Date(),
+    }
+    const venderDetials = await venderTrails.findByIdAndUpdate(id, { $set: updatedVenderTrails }, { new: true })
+    await payable.create({
       user: req.user.id,
       journalEntry: "dr",
       Description: venderDetials.venderName,
       amount: venderDetials.purchaseAmount,
+      id: venderDetials.productId,
     })
     await ledger.create({
       user: req.user.id,
       journalEntry: "cr",
       Description: "Payable",
-      amount:-venderDetials.purchaseAmount,
-      TransactionId:venderDetials.id.toString()
+      amount: -venderDetials.purchaseAmount,
+      TransactionId: venderDetials.id.toString(),
     })
-     
-      res.status(200).send({ status: 200, msg: "Updated" });
-    } catch (err) {
-      res.status(500).json(err);
-    }
+
+    res.status(200).send({ status: 200, msg: "Updated" })
+  } catch (err) {
+    res.status(500).json(err)
   }
-);
+})
 
 router.get("/fetchAllVenderTrails", validator, async (req, res) => {
   try {
-    const VenderTrails = await venderTrails.find({ user: req.user.id });
-    
-    res.status(200).json({ venderTrails: VenderTrails, status: 200 });
-  } catch (err) {
-    res.json(err);
-  }
-});
+    const VenderTrails = await venderTrails.find({ user: req.user.id })
 
-module.exports = router;
+    res.status(200).json({ venderTrails: VenderTrails, status: 200 })
+  } catch (err) {
+    res.json(err)
+  }
+})
+
+module.exports = router
