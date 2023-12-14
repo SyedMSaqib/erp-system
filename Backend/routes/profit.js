95d1784512ee6fb09ce8d9d624f7f314d307c144
Saqib <splasher733@gmail.com>
2023-12-14 05:36:44 +0500

    table added with proft, total transaction and total sales

--- Backend/routes/profit.js
+++ Backend/routes/profit.js
@@ -9,7 +9,7 @@ router.get("/profit/:month", validator, async (req, res) => {
     const monthName = req.params.month
     const monthIndex = new Date(Date.parse(`1 ${monthName} 2000`)).getMonth()
     const requestedMonth = monthIndex+1
-    console.log(requestedMonth)
+   
 
     const startDate = new Date(new Date().getFullYear(), requestedMonth - 1, 1)
     const endDate = new Date(new Date().getFullYear(), requestedMonth, 0, 23, 59, 59, 999)
@@ -24,13 +24,19 @@ router.get("/profit/:month", validator, async (req, res) => {
       user: req.user.id,
       date: { $gte: startDate, $lt: endDate },
     })
-
+    let totalSales=0
+    for(sales of saleTrails)
+    {
+        totalSales++
+    }
     let totalProfitLoss = 0
+    let totalTransaction= 0
     for (prof of Ledger) {
       totalProfitLoss += prof.amount
+      totalTransaction++
     }
 
-    res.status(200).json({ saleTrails: saleTrails, profit: totalProfitLoss, status: 200 })
+    res.status(200).json({ saleTrails: saleTrails, profit: totalProfitLoss,totalTransaction:totalTransaction,totalSales:totalSales, status: 200 })
   } catch (err) {
     res.status(500).json(err)
   }
--- src/components/profit/profit.js
+++ src/components/profit/profit.js
@@ -1,21 +1,67 @@
-import React from 'react'
-import ProfitChart from './profitChart'
-import MonthDropdown from './monthDropdown'
+import React, { useContext } from 'react';
+import ProfitChart from './profitChart';
+import MonthDropdown from './monthDropdown';
+import profitContext from "../../context/profit/profitContext";
 
 const Profit = () => {
-  return (
-    <div className='flex justify-center '>
-       <div className=" z-40 shadow-md w-screen h-[6rem]   bg-slate-100 bg-opacity-5 backdrop-blur-md   dark:bg-gray-950 dark:bg-opacity-5 dark:backdrop-blur-md fixed ">
-          <div className="flex justify-center items-center pt-2 pl-56">
-            <MonthDropdown />
-          </div>
-        </div>
-           <div className="absolute top-0 text-center ml-52 mt-[7rem] font-semibold text-lg dark:text-gray-300">Sales Profit</div>
-        <div className='flex justify-center ml-[10rem] mt-[10rem]'>
-          <ProfitChart/>
-        </div>
-    </div>
-  )
-}
+  const { profit, Month } = useContext(profitContext);
 
-export default Profit
+  return (
+    <div className='flex flex-col items-center dark:bg-gray-900 bg-slate-50'>
+      <div className="z-40 shadow-md w-screen h-[6rem] bg-slate-100 bg-opacity-5 backdrop-blur-md dark:bg-gray-950 dark:bg-opacity-5 dark:backdrop-blur-md fixed">
+        <div className="flex justify-center items-center pt-2">
+          <MonthDropdown />
+        </div>
+      </div>
+      <div className="text-center mt-[7rem] ml-[10rem]  font-semibold text-lg dark:text-gray-300">Sales Profit</div>
+      <div className='flex justify-center mt-5 ml-[10rem]'>
+        <ProfitChart />
+      </div>
+      <div className="shadow-lg m-5 ml-[14rem]">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:border-gray-700 dark:text-gray-400 border border-gray-300 ">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3 border-2">
+                Month
+              </th>
+              <th scope="col" className="px-10 py-3 border-2">
+                Total Transactions
+              </th>
+              <th scope="col" className="px-10 py-3 border-2">
+                Total Sales (Cleared)
+              </th>
+              {profit.profit > 0 ? (
+                <th scope="col" className="px-20 py-3 border-2">
+                  Total Profit (Sales-(Purchases+Expenses))
+                </th>
+              ) : (
+                <th scope="col" className="px-20 py-3 border-2">
+                  Total Loss (Sales-(Purchases+Expenses))
+                </th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              {profit && <td className="px-6 py-4 font-semibold border-2">{Month}</td>}
+              {profit && <td className="px-12 py-4 font-semibold border-2">{profit.totalTransaction}</td>}
+              {profit && <td className="px-12 py-4 font-semibold border-2">{profit.totalSales}</td>}
+              {profit && profit.profit > 0 ? (
+                <td className="px-40 py-4 font-semibold border-l">
+                  {Math.abs(profit.profit).toFixed(0)} Rs
+                </td>
+              ) : (
+                <td className="px-40 py-4 font-semibold text-red-600 border-l">
+                  {Math.abs(profit.profit).toFixed(0)} Rs
+                </td>
+              )}
+            </tr>
+          
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Profit;
