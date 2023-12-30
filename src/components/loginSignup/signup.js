9acf857627ca8a78dfb8c29af8912ebf074da735
Saqib <splasher733@gmail.com>
2023-12-31 03:42:17 +0500

    style: styling adjusted and added a route for login

--- src/components/loginSignup/signup.js
+++ src/components/loginSignup/signup.js
@@ -6,6 +6,7 @@ import SasLogo from '../sidebar/logos/sasLogo2.png'
 import Footer from '../loginSignup/footer'
 import SignUpSuccess from "./signUpSuccess";
 import toast from "react-hot-toast"
+import { Link } from "react-router-dom"
 
 
 const SignUp = () => {
@@ -106,24 +107,24 @@ const SignUp = () => {
       
         <div className="max-w-md w-full">
           
-          <div className="bg-gray-50 rounded-lg shadow-md p-6 md:p-8">
+          <div className="bg-gray-50 rounded-lg shadow-md p-6 ">
           <div className=' flex justify-center'>
 
          <img className="w-24" src={SasLogo} alt="Logo" />
           </div>
           
-          <h1 className="flex justify-center font-mono  text-sm  py-3 leading-tight tracking-tight text-gray-900 md:text-xl">
+          <h1 className="flex justify-center font-mono  text-sm   leading-tight tracking-tight text-gray-900 md:text-xl">
             SIGN UP
           </h1>
             <div className="">
               {validation === "email" ? (
-                <span className="text-red-700 block mb-2 text-sm font-medium">Enter correct email!</span>
-              ) : validation === "password" ? (
-                <span className="text-red-700 block mb-2 text-sm font-medium">Password minimum 8 characters!</span>
+                <span className="text-red-700 block  text-sm font-medium">Enter correct email!</span>
+                ) : validation === "password" ? (
+                <span className="text-red-700 block  text-sm font-medium">Password minimum 8 characters!</span>
               ) : validation === "name" ? (
-                <span className="text-red-700 block mb-2 text-sm font-medium">Enter Valid Name!</span>
+                <span className="text-red-700 block  text-sm font-medium">Enter Valid Name!</span>
               ) : emailAlreadyExists? (
-                <span className="text-red-700 block mb-2 text-sm font-medium">Email Aready Exists!</span>
+                <span className="text-red-700 block  text-sm font-medium">Email Aready Exists!</span>
               ):""}
               <label for="name" className="block mb-2 text-sm font-medium text-gray-900">
                 Your Name
@@ -133,7 +134,7 @@ const SignUp = () => {
                 type="name"
                 name="name"
                 id="name"
-                className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                className="bg-gray-100 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 mb-2"
                 placeholder="eg: Saqib"
                 required=""
               />
@@ -190,6 +191,12 @@ const SignUp = () => {
               >
                 Sign Up
               </button>
+              <div className="text-sm font-light text-gray-500">
+                    Already have an account?{" "}
+                    <Link to={"/signIn"} className="font-medium text-primary-600 hover:underline">
+                      Sign in
+                    </Link>
+                  </div>
             </form>
           </div>
         </div>
