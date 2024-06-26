5fe1b0407c02ca6e0f75140c18ffd1eb45897cd0
Saqib <Splasher733@gmail.com>
2024-06-26 22:54:19 +0500

    ...

--- Backend/middleware/validator.js
+++ Backend/middleware/validator.js
@@ -13,10 +13,10 @@ const validator = (req, res, next) => {
         const verifyToken = jwt.verify(token, "02192000");
         console.log(verifyToken)
 
-        // const currentTime = Math.floor(Date.now() / 1000);
-        // if (verifyToken.exp && verifyToken.exp < currentTime) {
-        //     return res.status(400).json({"Error": "Token is expired"});
-        // }
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (verifyToken.exp && verifyToken.exp < currentTime) {
+            return res.status(400).json({"Error": "Token is expired"});
+        }
 
         req.user = verifyToken.user;
         next();
