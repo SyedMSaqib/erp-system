d25ecbaf1bce83419bde31bb1b733396ef15778d
Saqib <Splasher733@gmail.com>
2024-06-26 22:29:53 +0500

    ...

--- Backend/dbConnection.js
+++ Backend/dbConnection.js
@@ -1,8 +1,6 @@
 
 const mongoose = require('mongoose');
-
-const uri='mongodb+srv://splasher733:PJa3W8PibxvQZ8s@cluster0.icjtgav.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-// const uri='mongodb://127.0.0.1:27017/Erp'
+const uri='mongodb://127.0.0.1:27017/Erp'
 
 
 
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-const config={ apiurl: "https://erp-xi-jade.vercel.app", }
-// const config={apiurl:"http://localhost:5000"}
+// const config={ apiurl: "https://erp-xi-jade.vercel.app", }
+const config={apiurl:"http://localhost:5000"}
 
 export default config;
 //
