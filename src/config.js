dc018414040b4351a6877496d241efda388b31fc
Saqib <Splasher733@gmail.com>
2024-06-26 21:05:15 +0500

    ..

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const config={ apiurl: "https://erp-xi-jade.vercel.app", }
+// const config={ apiurl: "https://erp-xi-jade.vercel.app", }
+const config={apiurl:"http://localhost:5000"}
 export default config;
 //
